41117ba0c4fcae1cbf9ea2082a220b39c5cdc23e
James O'Neal <jamesooo@protonmail.com>
2020-06-27 10:50:20 -0700

    installed python deps for spacemacs python plugins

--- init.sh
+++ init.sh
@@ -3,6 +3,7 @@ DOTFILES=$HOME/dotfiles
 
 # SPACEMACS
 ## install spacemacs
+pip3 install python-language-server[all] pyls-isort pyls-mypy pyls-black flake8 autoflake importmagic epc
 git clone -b develop https://github.com/syl20bnr/spacemacs ~/.emacs.d
 ln -s $DOTFILES/.spacemacs $HOME/.spacemacs
 
